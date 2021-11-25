0f0f60ace821053d253a86a06fd35f1c86d61f50
DasBabyPixel <dasbabypixel@gmaill.com>
2021-11-25 22:26:59 +0100

    Updated

--- core/src/main/java/labyrinth/Main.java
+++ core/src/main/java/labyrinth/Main.java
@@ -1,9 +1,10 @@
 package labyrinth;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		System.out.println("test");
+		System.out.println("test2");
+		System.out.println(Main.class.getClassLoader().getResourceAsStream("res1"));
 	}
-
 }
